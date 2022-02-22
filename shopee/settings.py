cd8a1b1a5942204d23de6e8b3ffa12a8a1354f18
Sharafudheen.M <sharafumthouse@gmail.com>
2022-02-22 06:54:07 -0800

    tk added

--- shopee/settings.py
+++ shopee/settings.py
@@ -13,7 +13,7 @@ import os
 import django_heroku
 
 from pathlib import Path
-
+from tkinter import E
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
